1ada0d7712224c133ed9e9c3875bbf818e7ba8db
Nikshay Nimje <148868704+niksnimje@users.noreply.github.com>
2024-10-25 11:47:33 +0000

    Okay Done

--- BOOK STORE DETAILS/Back-End/db.js
+++ BOOK STORE DETAILS/Back-End/db.js
@@ -5,16 +5,7 @@ const MONGOURL=process.env.MONGO_URL
 
     const connection =  mongoose.connect(MONGOURL);
     
-    const bookSchema = new mongoose.Schema({
-        title : String,
-        author : String,
-        price : Number,
-        description : String,
-        ISBN : String, 
-        image: String
-      },{ versionKey: false });
-      
-      const BookModal = mongoose.model('Book', bookSchema);
+   
 
 
-module.exports = {connection, BookModal} ;
+module.exports = {connection} ;
--- BOOK STORE DETAILS/Back-End/index.js
+++ BOOK STORE DETAILS/Back-End/index.js
@@ -2,6 +2,8 @@ const express=require("express")
 const cors = require('cors')
 
 const {connection, BookModal} = require("./db");
+const bookRoutes = require("./Contoroller/book.contoroller");
+
 
 
 const app=express()
@@ -9,78 +11,8 @@ app.use(cors())
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+app.use("/bookRoutes",bookRoutes)
 
-    res.send("okay")
-})
-
-// Add Books
-
-app.post('/addbooks', async (req, res) => {
-    try {
-      const { title, author, price, description ,ISBN , image } = req.body;
-      const book = new BookModal({ title, author, price, description,ISBN,image });
-
-      await book.save()
-      res.status(201).json(book); 
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  });
-
-
-  // Get Books
-
-  app.get('/getbooks', async (req, res) => {
-    try {
-      const book = await BookModal.find(); 
-      res.status(200).json(book);
-      // res.json(book)
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
-  
-
-  // Get User BY ID
-  app.get('/books/:id', async (req, res) => {
-    try {
-      const book = await BookModal.findById(req.params.id); // Retrieve user by ID
-      if (!book) return res.status(404).json({ message: 'Book not found' });
-      res.status(200).json(book);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
-
-  
-  
-  // UPDATE: Update a book by ID
-  app.patch('/book/:id', async (req, res) => {
-    try {
-      const { title, author, price, description, ISBN, image } = req.body;
-      const user = await BookModal.findByIdAndUpdate(
-        req.params.id,
-        { title, author, price, description, ISBN, image },
-       
-      );
-      res.status(200).json({message:"Book Detail's Updated !!"})
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  });
-  
-
-  // DELETE: Delete a book by ID
-  app.delete('/book/:id', async (req, res) => {
-    try {
-      const user = await BookModal.findByIdAndDelete(req.params.id); 
-     
-      res.status(200).json({ message: 'Book deleted successfully' });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
 
 
 
